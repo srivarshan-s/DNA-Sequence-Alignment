881af0b4f86eaffffaaab0db7a94a060103abbc6
Srivarshan Selvaraj <srivarshan02@gmail.com>
2024-05-06 12:28:08 -0700

    Bug fix: robustness of python path

--- code/basic.sh
+++ code/basic.sh
@@ -4,13 +4,13 @@
 python_path=$(which python)
 
 # Check if python is installed
-if [[ -z $python_path ]]; then
+if [[ -z "$python_path" ]]; then
     # If python is not installed point to python3 executable
     python_path=$(which python3)
 fi
 
 # If both are not installed throw error
-if [[ -z $python_path ]]; then
+if [[ -z "$python_path" ]]; then
     echo "Error: Python or Python3 not found!"
     exit 1
 fi
