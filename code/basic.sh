8f2a63b71c095726f1673bafecc65243fa85671b
Srivarshan Selvaraj <srivarshan02@gmail.com>
2024-05-06 10:51:25 -0700

    Add script to run basic implementation

---
+++ code/basic.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Get the path of the python executable
+python_path=$(which python)
+
+# Check if python is installed
+if [[ -z $python_path ]]; then
+    # If python is not installed point to python3 executable
+    python_path=$(which python3)
+fi
+
+# If both are not installed throw error
+if [[ -z $python_path ]]; then
+    echo "Error: Python or Python3 not found!"
+    exit 1
+fi
+
+$python_path basic_3.py $1 $2
